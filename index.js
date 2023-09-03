da3ab3b1de69ffb98801dde63c8eec67f7a1dd6f
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-09-03 11:03:15 -0400

    Add ID field to start and stop

--- index.js
+++ index.js
@@ -34,55 +34,6 @@ function log(text) {
     }
 }
 
-// io.on('connection', socket => {
-//     log("client connected to socket server " + socket.id);
-//     socket.on('start', (lat, lng) => {
-//         log(`Received start message ${lat} ${lng}`);
-//         io.emit('start', lat, lng);
-//     });
-//     socket.on('stop', () => {
-//         log("Received stop message");
-//         io.emit('stop');
-//     });
-//     socket.on('device-1', (receivedData1) => {
-//         let receivedData = receivedData1;
-//         if (typeof receivedData === 'string' || receivedData instanceof String) {
-//             try {
-//                 receivedData = JSON.parse(receivedData);
-//             } catch (e) {
-//                 log("Unable to convert device data to JSON object: " + receivedData1);
-//                 return;
-//             }
-//         }
-//         log("received data from device: " + JSON.stringify(receivedData));
-//         if (!receivedData) {
-//             log("Undefined data");
-//             return;
-//         }
-//         if (!receivedData["Position"]) {
-//             log("Data missing position");
-//             return;
-//         }
-//         if (receivedData["Position"]["x"] === undefined || receivedData["Position"]["y"] === undefined || receivedData["Position"]["z"] === undefined) {
-//             log("Position data missing either x, y, or z");
-//             return;
-//         }
-//         if (!receivedData["Orientation"]) {
-//             log("Data missing orientation");
-//             return;
-//         }
-//         if (receivedData["Orientation"]["x"] === undefined || receivedData["Orientation"]["y"] === undefined || receivedData["Orientation"]["z"] === undefined || receivedData["Orientation"]["w"] === undefined) {
-//             log("Orientation data missing either x, y, z, or w");
-//             return;
-//         }
-//         if (!receivedData["Confidence"]) {
-//             log("Data missing confidence");
-//             return;
-//         }
-//         io.emit('device-data', receivedData);
-//     });
-// });
-
 server.listen(PORT, () => {
     log(`App listening on the port ${PORT}`);
 }).on('error', e => {
--- public/index.js
+++ public/index.js
@@ -67,20 +67,23 @@ function createCenterControl(textContent, map, cb) {
 function addButtonsToMap(map) {
     const centerControlDiv = document.createElement("div");
     const startBtn = createCenterControl("Start", map, (_map) => {
-        let inputText = window.prompt("Please input latitude, longitude (ex: '55.55, 66.66')", "");
-        let latLng = inputText?.split(",");
-        if (latLng && latLng.length >= 2) {
-            const lat = parseFloat(latLng[0]?.trim() || 0.00);
-            const lng = parseFloat(latLng[1]?.trim() || 0.00);
-            socket.emit("start", lat, lng);
+        let inputText = window.prompt("Please input latitude, longitude, altitude, and unit ID (ex: '55.55, 66.66, 77.77, unit-1')", "");
+        let data = inputText?.split(",");
+        if (data && data.length >= 4) {
+            const lat = parseFloat(data[0]?.trim() || 0.00);
+            const lng = parseFloat(data[1]?.trim() || 0.00);
+            const alt = parseFloat(data[2]?.trim() || 0.00);
+            const id = data[3]?.trim() || "no_id";
+            socket.emit("start", lat, lng, alt, id);
         } else {
             console.error("Input is not valid");
             alert("Input is not valid");
         }
     });
     const stopBtn = createCenterControl("Stop", map, (_map) => {
-        socket.emit("stop");
-        alert("stop");
+        let inputText = window.prompt("Please input unit ID (ex: 'unit-1')", "");
+        const id = inputText?.trim() || "no_id";
+        socket.emit("stop", id);
     });
     centerControlDiv.appendChild(startBtn);
     centerControlDiv.appendChild(stopBtn);
@@ -108,6 +111,7 @@ socket.on("device-data", (data) => {
     const lat = data?.Position?.x;
     const lng = data?.Position?.y;
     const altitude = data?.Position?.z;
+    const id = data?.ID;
 
     console.log('Received data from socket (x is lat, y is lng, z is altitude)', data);
     if (lat && lng && altitude) {
@@ -163,7 +167,6 @@ function initWebglOverlayView(map) {
         loader.manager.onLoad = () => {
             renderer.setAnimationLoop(() => {
                 webglOverlayView.requestRedraw();
-                // latLngAltitudeLiteral.altitude += 0.1;
             });
         };
     };
